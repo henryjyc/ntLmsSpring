99b754ca059d15aed8451729152b281dbd98a7f9
Jonathan Lovelace <kingjon3377@gmail.com>
2019-05-09 20:44:20 -0400

    SpringlmsApplication: Allow cross-origin calls to this service.

--- src/main/java/com/st/novatech/springlms/SpringlmsApplication.java
+++ src/main/java/com/st/novatech/springlms/SpringlmsApplication.java
@@ -2,7 +2,10 @@ package com.st.novatech.springlms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Main driver class for the application for running from the command line.
@@ -13,10 +16,26 @@ public class SpringlmsApplication {
 
 	/**
 	 * Main method.
+	 *
 	 * @param args parsed by Spring
 	 */
 	public static void main(final String[] args) {
 		SpringApplication.run(SpringlmsApplication.class, args);
 	}
 
+	/**
+	 * Enable cross-origin requests.
+	 *
+	 * @return the CORS filter
+	 */
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(final CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT",
+						"DELETE", "HEAD");
+			}
+		};
+	}
 }
